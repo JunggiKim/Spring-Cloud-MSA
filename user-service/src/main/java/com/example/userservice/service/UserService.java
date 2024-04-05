8c5f6fb932d79a86e2e5e424fc7661c616e7aebb
superstart <k42ke@naver.com>
2024-04-05 17:44:21 +0900

    user-service 스프링 시큐리티 작업 진행 시작

---
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
+
+
+// extends UserDetailsService {
+public interface UserService {
+	UserDto createUser(UserDto userDto);
+
+	UserDto getUserByUserId(String userId);
+	Iterable<UserEntity> getUserByAll();
+
+	UserDto getUserDetailsByEmail(String userName);
+}
